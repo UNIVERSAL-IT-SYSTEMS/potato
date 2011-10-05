b30de0769da665b53f759859a0bf564fc8d57e69
Markus Berg <markus@kelvin.nu>
2011-10-05 07:58:48 +0200

    Bug fix to schema definition. Add proper constraint.

--- mossad_schema.sql
+++ mossad_schema.sql
@@ -8,14 +8,15 @@ CREATE TABLE IF NOT EXISTS `User` (
   `secret` varchar(32) NOT NULL,
   `pin` char(8),
   `invalidLogins` tinyint(1) not null default 0,
-  PRIMARY KEY  (`UserName`)
+  PRIMARY KEY  (`userName`)
 ) ENGINE=InnoDB CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `Guest` (
   `userName` char(16) NOT NULL,
   `password` varchar(32) NOT NULL,
   `dateCreation` timestamp default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`UserName`)
+  PRIMARY KEY  (`userName`),
+  CONSTRAINT `fkUserNameGuest` FOREIGN KEY (`userName`) references `User` (`userName`) on delete cascade
 ) ENGINE=InnoDB CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `Log` (
