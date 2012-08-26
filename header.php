1aee34b474210285e7e86aef1c0b9764ae758386
Markus Berg <markus@kelvin.nu>
2012-08-26 19:23:48 +0000

    More style fixes to make the colors nicer, and upped the version info in header.php

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
         <meta http-equiv="X-UA-Compatible" content="IE=9" /> 
         <link href="style.css?version=7" rel="stylesheet" type="text/css" />
-        <script type="text/javascript" src="main.js?version=6"></script>
+        <script type="text/javascript" src="main.js?version=7"></script>
         <title>One-time password configuration</title>
     </head>
 
