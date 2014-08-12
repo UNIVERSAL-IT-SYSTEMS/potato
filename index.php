fe8a62321401fd15967000f0e93b0d4ed2570386
Markus Berg <markus@kelvin.nu>
2014-08-12 15:52:51 +0200

    Factor out inline javascript to add it to header.

--- index.php
+++ index.php
@@ -56,80 +56,68 @@ if ( isset($_POST['action']) ) {
     }
 }
 
-$page->printHeader();
-?>
+function addVisibilityToggle($control, $id) {
+    global $page;
+    $page->addJsOnLoad(sprintf('document.getElementById("%s").addEventListener("click", function(event) { toggleVisibility("%s"); event.preventDefault(); event.returnValue=false; });', $control, $id));
+}
 
-<p>In order to use the Mobile OTP service, you must configure your mobile.
-Click for detailed instructions for your phone:
-    <ul>
-        <li><a href="#" onclick="toggleVisibility('android'); return(false);">Android</a>
-            <div class="instructions" id="android">
-              <div class="closebutton" onclick="toggleVisibility('android');"></div>
-              <div class="instructioncontents">
-                <h2>Android</h2>
-                <p>Install the <a href="https://play.google.com/store/apps/details?id=nu.kelvin.potato">Potato</a> 
-                  client app from Google play (previously Android Market). If you're unable to locate the 
-                  app, simply input this link <a href="http://bit.ly/NZo8dK">bit.ly/NZo8dK</a> 
-                  in your Android web browser, and you'll be redirected to the 
+function getPopup($id, $linktext, $title, $contents) {
+    addVisibilityToggle("ctr-$id", $id);
+    addVisibilityToggle("btn-$id", $id);
+    $retval = sprintf('<li><a href="#" id="ctr-%s">%s</a>' . "\n", $id, $linktext);
+    $retval .= sprintf('<div class="instructions" id="%s">' . "\n", $id);
+    $retval .= sprintf('<div class="closebutton" id="btn-%s"></div>' . "\n", $id);
+    $retval .= '<div class="instructioncontents">' . "\n";
+    $retval .= sprintf('<h2>%s</h2>'."\n", $title);
+    $retval .= $contents;
+    $retval .= '    <p>Setup is now complete. Proceed to the <a href="testtoken.php">token testing area</a> to test your new token</p>' . "\n";
+    $retval .= "</div>\n";
+    $retval .= "</div>\n";
+    $retval .= "</li>\n";
+    return $retval;
+}
+
+$instructions = getPopup('android', 'Android', 'Android',
+'                <p>Install the <a href="https://play.google.com/store/apps/details?id=nu.kelvin.potato">Potato</a>
+                  client app from Google play (previously Android Market). If you\'re unable to locate the
+                  app, simply input this link <a href="http://bit.ly/NZo8dK">bit.ly/NZo8dK</a>
+                  in your Android web browser, and you\'ll be redirected to the
                   app. Once installed, follow these steps:</p>
                 <ol>
-                  <li>Click the "Add profile" button (in Android 2.x, begin by clicking the menu button), and choose a memorable name for your token. For example: "<?php echo $orgName ?>"</li>
+                  <li>Click the "Add profile" button (in Android 2.x, begin by clicking the menu button), and choose a memorable name for your token.
+                    For example: "' . $orgName . '"</li>
                   <li>Your phone will also display a 16 character "Secret". Enter the secret in this web interface. Do not write this secret down anywhere else.</li>
                   <li>Click "Save profile"</li>
                   <li>Register a PIN in this web interface</li>
-                </ol>
-                <p>Setup is now complete. Proceed to the <a href="testtoken.php">token testing area</a> to test your new token</p>
-              </div>
-            </div>
-        </li>
+                </ol>');
 
-        <li><a href="#" onclick="toggleVisibility('iphone'); return(false);">iPhone</a>
-            <div class="instructions" id="iphone">
-              <div class="closebutton" onclick="toggleVisibility('iphone');"></div>
-              <div class="instructioncontents">
-                <h2>iPhone</h2>
-                <p>Install the <a href="http://itunes.apple.com/us/app/mobile-otp/id328973960&mt=8">iOTP</a> app from app store, and follow these steps:</p>
+$instructions .= getPopup('iphone', 'iPhone', 'iPhone',
+'                <p>Install the <a href="http://itunes.apple.com/us/app/mobile-otp/id328973960&mt=8">iOTP</a> app from app store, and follow these steps:</p>
                 <ol>
                   <li>On first start of the app, you will be prompted to create an account</li>
-                  <li>Select a memorable name for your account; for example "<?php echo $orgName ?>"</li>
+                  <li>Select a memorable name for your account; for example "' . $orgName . '"</li>
                   <li>Click "Generate secret", and select either 16 or 32 characters</li>
                   <li>Your phone will now display a 16 (or 32) character "Secret". Enter the secret in this web interface. Do not write this secret down anywhere else.</li>
                   <li>Register a PIN in this web interface</li>
-                </ol>
-                <p>Setup is now complete. Proceed to the <a href="testtoken.php">token testing area</a> to test your new token</p>
-              </div>
-            </div>
-        </li>
+                </ol>');
 
-        <li><a href="#" onclick="toggleVisibility('midlet'); return(false);">JAVA MIDlet compatible phone</a> (such as older non-smartphone SonyEricsson or Nokias)
-            <div class="instructions" id="midlet">
-              <div class="closebutton" onclick="toggleVisibility('midlet');"></div>
-              <div class="instructioncontents">
-                <h2>Java MIDlet</h2>
-                <p>Start the web browser on your phone, and enter the following address:<br />
-                <pre>http://motp.sf.net/MobileOTP.jar</pre>
-                <em>(note: capitalization matters)</em></p>
-
-                <p>Confirm that you want to install the program. Once installed, the app will be prompting you for your PIN code. In order to initialize the app for use, follow these steps:</p>
+$instructions .= getPopup('midlet', 'Java MIDlet compatible phone (such as older non-smartphone SonyEricsson, Nokia, or Blackberry phones)', 'Java MIDlet',
+'                <p>Install the <a href="http://itunes.apple.com/us/app/mobile-otp/id328973960&mt=8">iOTP</a> app from app store, and follow these steps:</p>
                 <ol>
-                  <li>Enter PIN code <strong>"0000"</strong></li>
-                  <li>The token should now prompt you for <strong>"25 random keys"</strong>. Go ahead and enter 25 random numbers and klick "Ok"</li>
-                  <li>The token now presents your 16 character "Init-Secret" in the display. Enter that secret into this web interface. Do not write this secret down anywhere else.</li>
-                  <li>Once you have entered a "Secret", you must also select a PIN. This action is also performed in this web interface</li>
-                </ol>
-                <p>Setup is now complete. Proceed to the <a href="testtoken.php">token testing area</a> to test your new token</p>
-              </div>
-            </div>
-        </li>
-        <li><a href="#" onclick="toggleVisibility('yubikey'); return(false);">Yubikey dongle</a>
-            <div class="instructions" id="yubikey">
-              <div class="closebutton" onclick="toggleVisibility('yubikey');"></div>
-              <div class="instructioncontents">
+                  <li>On first start of the app, you will be prompted to create an account</li>
+                  <li>Select a memorable name for your account; for example "' . $orgName . '"</li>
+                  <li>Click "Generate secret", and select either 16 or 32 characters</li>
+                  <li>Your phone will now display a 16 (or 32) character "Secret". Enter the secret in this web interface. Do not write this secret down anywhere else.</li>
+                  <li>Register a PIN in this web interface</li>
+                </ol>');
 
-                <h2>Yubikey administration</h2>
-                <p>For users that do not have a mobile phone for use as a token, 
+
+$page->addJsOnLoad('document.getElementById("yubikeyDetailShow").addEventListener("click", function(event) { setVisibility("yubikeyDetail", true); event.preventDefault(); event.returnValue=false; });');
+$page->addJsOnLoad('document.getElementById("yubikeyDetailHide").addEventListener("click", function(event) { setVisibility("yubikeyDetail", false); });');
+$instructions .= getPopup('yubikey', 'Yubikey dongle', 'Yubikey',
+'                <p>For users that do not have a mobile phone for use as a token,
                   standard HOTP tokens are also supported; for instance the
-                  <a href="http://www.yubico.com/">Yubikey</a>. Here are 
+                  <a href="http://www.yubico.com/">Yubikey</a>. Here are
                   step-by-step instructions for initializing your Yubikey
                   for first time use.</p>
 
@@ -137,11 +125,11 @@ Click for detailed instructions for your phone:
                   <li>Download and install the <a href="http://www.yubico.com/personalization-tool">Yubikey personalization tool from the Yubico site</a></li>
                   <li>Start the tool and select the OATH-HOTP mode</li>
                   <li>Select the "Quick" programming mode<br />
-                    <div id="yubikeyDetail" class="photobox" onclick="setVisibility('yubikeyDetail', false);">
-                      <img src="images/yubikey.png" />
+                    <div id="yubikeyDetail" class="photobox">
+                      <img src="images/yubikey.png" id="yubikeyDetailHide" alt="" />
                     </div>
-                    <a href="images/yubikey.png" onclick="setVisibility('yubikeyDetail', true); return(false);"><img src="images/yubikey_thumb.jpg"><br />
-                    (click for larger picture)</a>
+                    <a href="images/yubikey.png" id="yubikeyDetailShow"><img src="images/yubikey_thumb.jpg" alt=""><br />
+                      (click for larger picture)</a>
                   </li>
                   <li>Select "Configuration Slot 1"</li>
                   <li>Deselect the "OATH Token Identifier"</li>
@@ -153,38 +141,32 @@ Click for detailed instructions for your phone:
                 </ol>
 
                 <h2>Important</h2>
-                <p>As the HOTP tokens don't have PIN-codes, you need to enter your
+                <p>As the HOTP tokens don\'t have PIN-codes, you need to enter your
                   PIN as the first part of your passphrase when logging in.
                   For example: if your PIN-code is <strong>"1234"</strong>, you need to focus
                   the password field, and enter "1234" before pressing the Yubikey button for a total
-                  password length of 10 characters.
-                </p>
-              </div>
-            </div>
-        </li>
+                  password length of 10 characters.</p>
+');
 
-        <li><a href="#" onclick="toggleVisibility('winphone'); return(false);">Windows Phone</a>
-            <div class="instructions" id="winphone">
-              <div class="closebutton" onclick="toggleVisibility('winphone');"></div>
-              <div class="instructioncontents">
-                <h2>Windows Phone</h2>
-                <p>Install the <a href="http://www.windowsphone.com/en-US/apps/4eca404c-4936-4295-819a-8b4b85bcd592">Yamotp</a> app from the Windows Phone Marketplace, and follow these steps:</p>
+$instructions .= getPopup('winphone', 'Windows Phone', 'Windows Phone',
+'                <p>Install the <a href="http://www.windowsphone.com/en-US/apps/4eca404c-4936-4295-819a-8b4b85bcd592">Yamotp</a> app from the Windows Phone Marketplace, and follow these steps:</p>
                 <ol>
                   <li>Select "add profile"</li>
-                  <li>Select a memorable name for your token. For example: "<?php echo $orgName ?>"</li>
+                  <li>Select a memorable name for your token. For example: "' . $orgName . '"</li>
                   <li>Your phone will auto-initialize the 16 character "Secret Key". Enter the secret in this web interface. Do not write this secret down anywhere else.</li>
                   <li>Once your secret is safely registered here, click the "Save" button" on your phone.</li>
                   <li>Register a PIN in this web interface</li>
                 </ol>
-                <p>Setup is now complete. Proceed to the <a href="testtoken.php">token testing area</a> to test your new token</p>
-              </div>
-            </div>
-        </li>
+');
 
-    </ul>
-</p>
+$page->printHeader();
 
-<?php
+echo '<p>In order to use the Mobile OTP service, you must configure your mobile.
+Click for detailed instructions for your phone:
+    <ul>';
+echo $instructions;
+echo "    </ul>\n";
+echo "  </p>\n";
 
 if ( $user->hasToken() ) {
     echo '<div id="infoSecret">' . "\n";
